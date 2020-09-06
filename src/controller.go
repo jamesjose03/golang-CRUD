f1a27a87d380e22afbc4c48cbedd8c0b43193f65
jamesjose03 <jamesjose03@gmail.com>
2020-09-06 12:47:25 +0530

    feat: add user model

---
+++ src/controller.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// struct for storing data
+type user struct {
+	Name string `json:name`
+	Age  int    `json:age`
+	City string `json:city`
+}
