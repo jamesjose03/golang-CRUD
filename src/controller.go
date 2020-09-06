7cede6322887d96dc9f2f1035915008169daba88
jamesjose03 <jamesjose03@gmail.com>
2020-09-06 12:48:34 +0530

    feat: add insert user controller

--- src/controller.go
+++ src/controller.go
@@ -19,3 +19,26 @@ type user struct {
 	Age  int    `json:age`
 	City string `json:city`
 }
+
+var userCollection = db().Database("goTest").Collection("users") // get collection "users" from db() which returns *mongo.Client
+
+// Create Profile or Signup
+
+func createProfile(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json") // for adding Content-type
+
+	var person user
+	err := json.NewDecoder(r.Body).Decode(&person) // storing in person variable of type user
+	if err != nil {
+		fmt.Print(err)
+	}
+	insertResult, err := userCollection.InsertOne(context.TODO(), person)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult)
+	json.NewEncoder(w).Encode(insertResult.InsertedID) // return the mongodb ID of generated document
+
+}
